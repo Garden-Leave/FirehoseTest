919dae06f8f076b5724c0352d09b2d618a4ae8ad
sensetime <dgf526@msn.com>
2025-03-04 16:06:43 +0400

    update code

--- app_realtime.py
+++ app_realtime.py
@@ -7,7 +7,6 @@ apiKey = '12D7272397E44873BF36A60DB4C0DF33'
 apiKey_eu = '300207A2FA4D459789D4737FB73BFE49'
 api_url_io = 'https://partners.dnaspaces.io/api/partners/v1/firehose/events'
 api_url_eu = 'https://partners.dnaspaces.eu/api/partners/v1/firehose/events'
-
 test_api_url = 'http://127.0.0.1:5002/stream_out'
 
 
@@ -34,40 +33,56 @@ def stream():
                     eventType = event['eventType']
                     print(eventType)
                     if eventType == "IOT_TELEMETRY":
-                        try:
-                            ble_info = {}
-                            ble_info['device_mac'] = event['iotTelemetry']['deviceInfo']['deviceMacAddress']
-                            ble_info['ibeacon_uuid'] = event['iotTelemetry']['iBeacon']['uuid']
-                            ble_info['ibeacon_mac'] = event['iotTelemetry']['iBeacon']['beacon_mac_address']
-                            ble_info['ibeacon_major'] = event['iotTelemetry']['iBeacon']['major']
-                            ble_info['ibeacon_minor'] = event['iotTelemetry']['iBeacon']['minor']
-                            # ble_info['ibeacon_last_action'] = event['iotTelemetry']['last_user_action']['type']
-                            # # ble_info['ibeacon_last_action'] = 'dummy'
-                            # ble_info['ibeacon_battory'] = event['iotTelemetry']['battery']['value']
-                            # ble_info['ibeacon_x'] = event['iotTelemetry']['detectedPosition']['x_pos']
-                            # ble_info['ibeacon_y'] = event['iotTelemetry']['detectedPosition']['y_pos']
-                            # # ble_info['ibeacon_map_id'] = event['iotTelemetry']['DevicePosition']['map_id']
-                            # ble_info['ibeacon_location_id'] = event['iotTelemetry']['DevicePosition']['location_id']
-                            # ble_info['ibeacon_last_seen'] = event['iotTelemetry']['DevicePosition']['last_located_time']
-
-                            if ble_info['ibeacon_uuid'] == 'FDA50693-A4E2-4FB1-AFCF-C6EB07647825':
-                                ble_info['brand'] = 'Sensetime'
-                                print('''sensetime ble tag found, uuid: {} mac: {} major: {} minor: {}'''
-                                      .format(ble_info['ibeacon_uuid'], ble_info['ibeacon_mac'],
-                                              ble_info['ibeacon_major'], ble_info['ibeacon_minor']))
-                            # 不encode的话还是string 需要转化为字节流
-                            resp = json.dumps(ble_info).encode('utf-8')
-                            yield resp + b'\n'
+                        # print(event)
+                        ble_info = {}
+                        ble_info['device_mac'] = event['iotTelemetry']['deviceInfo']['deviceMacAddress']
+                        ble_info['ibeacon_location_x'] = event['iotTelemetry']['detectedPosition']['xPos']
+                        ble_info['ibeacon_location_y'] = event['iotTelemetry']['detectedPosition']['yPos']
+                        try: #判断BLE类型  是否ibeacon
+                            ibeacon_flag= event['iotTelemetry']['iBeacon']
                         except KeyError as e:
-                            print(f"Missing key in event data: {e}")
-                            continue  # Skip this event and continue
+                            print(f"ibeacon filed not existing: {e}")
+                            ble_info['brand']='BLE device but not iBeacon'
                         except json.JSONDecodeError as e:
                             print(f"Error decoding JSON: {e}")
-                            continue  # Skip this event and continue
                         except Exception:
-                            continue
+                            print("Unknown exception occurred")
+                        else:
+                            print(event)
+                            if event['iotTelemetry']['iBeacon']['uuid'] == "fda50693-a4e2-4fb1-afcf-c6eb07647825":
+                                ble_info['ibeacon_uuid'] = event['iotTelemetry']['iBeacon']['uuid']
+                                ble_info['ibeacon_mac'] = event['iotTelemetry']['iBeacon']['beaconMacAddress']
+                                ble_info['ibeacon_major'] = event['iotTelemetry']['iBeacon']['major']
+                                ble_info['ibeacon_minor'] = event['iotTelemetry']['iBeacon']['minor']
+                                ble_info['brand'] = 'Sensetime Beacon'
+                            else:
+                                ble_info['brand'] = 'iBeacon device but other brand'
+                            # print('''sensetime ble tag found, uuid: {} mac: {} major: {} minor: {}'''
+                            #       .format(ble_info['ibeacon_uuid'], ble_info['ibeacon_mac'],
+                            #               ble_info['ibeacon_major'], ble_info['ibeacon_minor']))
+                        finally:
+                            yield json.dumps(ble_info).encode('utf-8') + b'\n\n'
+                        # ble_info['ibeacon_last_action'] = event['iotTelemetry']['last_user_action']['type']
+                        # # ble_info['ibeacon_last_action'] = 'dummy'
+                        # ble_info['ibeacon_battory'] = event['iotTelemetry']['battery']['value']
+
+                        # # ble_info['ibeacon_map_id'] = event['iotTelemetry']['DevicePosition']['map_id']
+                        # ble_info['ibeacon_location_id'] = event['iotTelemetry']['DevicePosition']['location_id']
+                        # ble_info['ibeacon_last_seen'] = event['iotTelemetry']['DevicePosition']['last_located_time']
+
+                        # # 假设只有FDA50693-A4E2-4FB1-AFCF-C6EB07647825这个uuid是Sensetime
+                        # if ble_info['ibeacon_uuid'] == 'FDA50693-A4E2-4FB1-AFCF-C6EB07647825':
+                        #     ble_info['brand'] = 'Sensetime'
+                        #     print('''sensetime ble tag found, uuid: {} mac: {} major: {} minor: {}'''
+                        #           .format(ble_info['ibeacon_uuid'], ble_info['ibeacon_mac'],
+                        #                   ble_info['ibeacon_major'], ble_info['ibeacon_minor']))
+
+                        # 不encode的话还是string 需要转化为字节流bytes
+                        # resp = json.dumps(ble_info).encode('utf-8')
+                        # yield resp + b'\n'
                     else:
-                        yield json.dumps(event).encode('utf-8') + b'\n\n'
+                        # yield json.dumps(event).encode('utf-8') + b'\n\n'
+                        yield "Non-IoT event, Will not Parse\n\n"# 非IOT 什么也不输出
         else:
             print('request failed not 200')
             yield f"Error: {response.status_code}"
