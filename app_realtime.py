35e6be7cff28bd07e1dde3d603d9395ef305e00d
sensetime <dgf526@msn.com>
2025-05-05 15:38:54 +0400

    update ibeacon filtering

--- app_realtime.py
+++ app_realtime.py
@@ -1,6 +1,7 @@
 from flask import  Flask,render_template,session,Response
 import requests,json
-
+from datetime import datetime,timedelta
+from zoneinfo import ZoneInfo
 app = Flask('read_and_display')
 
 apiKey = '12D7272397E44873BF36A60DB4C0DF33'
@@ -9,6 +10,7 @@ api_url_io = 'https://partners.dnaspaces.io/api/partners/v1/firehose/events'
 api_url_eu = 'https://partners.dnaspaces.eu/api/partners/v1/firehose/events'
 test_api_url = 'http://127.0.0.1:5002/stream_out'
 
+stuuid = 'fda50693a4e24fb1afcfc6eb07647825'
 
 @app.route('/',endpoint='iot')
 def main():
@@ -31,37 +33,71 @@ def stream():
                     event = json.loads(decoded_line)
                     # print(event)
                     eventType = event['eventType']
-                    print(eventType)
+                    # print(eventType)
                     if eventType == "IOT_TELEMETRY":
                         # print(event)
                         ble_info = {}
-                        ble_info['device_mac'] = event['iotTelemetry']['deviceInfo']['deviceMacAddress']
-                        ble_info['ibeacon_location_x'] = event['iotTelemetry']['detectedPosition']['xPos']
-                        ble_info['ibeacon_location_y'] = event['iotTelemetry']['detectedPosition']['yPos']
+                        info1 = {}
+                        # ble_info['device_mac'] = event['iotTelemetry']['deviceInfo']['deviceMacAddress']
+                        # ble_info['ibeacon_location_x'] = event['iotTelemetry']['detectedPosition']['xPos']
+                        # ble_info['ibeacon_location_y'] = event['iotTelemetry']['detectedPosition']['yPos']
+
                         try: #判断BLE类型  是否ibeacon
                             ibeacon_flag= event['iotTelemetry']['iBeacon']
                         except KeyError as e:
-                            print(f"ibeacon filed not existing: {e}")
+                            print(f"ibeacon field not existing: {e}")
                             ble_info['brand']='IoT event but not iBeacon'
                         except json.JSONDecodeError as e:
                             print(f"Error decoding JSON: {e}")
                         except Exception:
                             print("Unknown exception occurred")
                         else:
-                            print(event)
-                            if event['iotTelemetry']['iBeacon']['uuid'] == "fda50693a4e24fb1afcfc6eb07647825":
-                                ble_info['ibeacon_uuid'] = event['iotTelemetry']['iBeacon']['uuid']
-                                ble_info['ibeacon_mac'] = event['iotTelemetry']['iBeacon']['beaconMacAddress']
-                                ble_info['ibeacon_major'] = event['iotTelemetry']['iBeacon']['major']
-                                ble_info['ibeacon_minor'] = event['iotTelemetry']['iBeacon']['minor']
-                                ble_info['brand'] = 'Sensetime Beacon'
+                            # print(event)
+                            if event['iotTelemetry']['iBeacon']['uuid'] == stuuid:
+                                # major 20103  minor  21968
+                                major = event['iotTelemetry']['iBeacon']['major']
+                                minor = event['iotTelemetry']['iBeacon']['minor']
+                                # print(major,minor)
+                                info1['x'] = event['iotTelemetry']['detectedPosition']['xPos']
+                                info1['y'] = event['iotTelemetry']['detectedPosition']['yPos']
+                                info1['uuid'] = stuuid
+                                info1['mac'] = event['iotTelemetry']['iBeacon']['beaconMacAddress']
+                                info1['major'] = str(major)
+                                info1['minor'] = str(minor)
+                                ts1 = float(event['iotTelemetry']['detectedPosition'][
+                                               'lastLocatedTime']) / 1000
+                                ts2 = datetime.utcfromtimestamp(ts1)
+                                delta = timedelta(hours=4)
+                                last_time=ts2 + delta
+                                info1['last_seen'] = str(last_time)
+                                info1['rssi'] = event['iotTelemetry']['maxDetectedRssi']
+                                # if  major == '20103' and ['minor'] in ['22613','23415']:
+                                if major == 20103 and minor == 21988:
+                                # ble_info['ibeacon_uuid'] = event['iotTelemetry']['iBeacon']['uuid']
+                                # ble_info['ibeacon_mac'] = event['iotTelemetry']['iBeacon']['beaconMacAddress']
+                                # ble_info['ibeacon_major'] = event['iotTelemetry']['iBeacon']['major']
+                                # ble_info['ibeacon_minor'] = event['iotTelemetry']['iBeacon']['minor']
+                                # ble_info['brand'] = 'Sensetime Beacon'
+                                # ble_info['ibeacon_last_seen'] = event['iotTelemetry']['detectedPosition'][
+                                #     'lastLocatedTime']
+                                    try:
+                                        print(info1)
+                                        yield json.dumps(info1).encode('utf-8') + b'\n\n'
+                                    except TypeError as e:
+                                        print(e)
+                                else:
+                                    yield "-"
                             else:
-                                ble_info['brand'] = 'iBeacon device but not Sensetime'
+                                yield "*"
+                            # else:
+                            #     ble_info['brand'] = 'iBeacon device but not Sensetime'
                             # print('''sensetime ble tag found, uuid: {} mac: {} major: {} minor: {}'''
                             #       .format(ble_info['ibeacon_uuid'], ble_info['ibeacon_mac'],
                             #               ble_info['ibeacon_major'], ble_info['ibeacon_minor']))
                         finally:
-                            yield json.dumps(ble_info).encode('utf-8') + b'\n\n'
+                            # yield json.dumps(ble_info).encode('utf-8') + b'\n\n'
+                            # yield json.dumps(info1).encode('utf-8') + b'\n\n'
+                            pass
                         # ble_info['ibeacon_last_action'] = event['iotTelemetry']['last_user_action']['type']
                         # # ble_info['ibeacon_last_action'] = 'dummy'
                         # ble_info['ibeacon_battory'] = event['iotTelemetry']['battery']['value']
@@ -82,9 +118,9 @@ def stream():
                         # yield resp + b'\n'
                     else:
                         # yield json.dumps(event).encode('utf-8') + b'\n\n'
-                        yield "Non-IoT event, Will not Parse\n\n"# 非IOT 什么也不输出
+                        yield "+"# 非IOT 什么也不输出
         else:
-            print('request failed not 200')
+            print('request failed')
             yield f"Error: {response.status_code}"
 
 
